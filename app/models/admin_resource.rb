c66d1dd4226c402bc67f72726d9f7a6eedb99441
krongk <kenrome@gmail.com>
2014-10-17 17:27:28 +0800

    Fewaf

--- app/models/admin_resource.rb
+++ app/models/admin_resource.rb
@@ -1,4 +1,8 @@
 class AdminResource < ActiveRecord::Base
   has_attached_file :asset, :default_url => "/assets/:style/missing.png"
   #validates_attachment_content_type :asset, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_presence :asset
+  validates_attachment_size :asset, :less_than => 50.megabytes
+  #validates_attachment_content_type :asset, :content_type => /\Aimage\/.*\Z/
+  do_not_validate_attachment_file_type :asset
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,7 +3,9 @@ class Resource < ActiveRecord::Base
   has_attached_file :asset,
                     :path => ":class/:id/:style.:extension"
 
+  validates_attachment_presence :asset
   validates_attachment_size :asset, :less_than => 50.megabytes
   #validates_attachment_content_type :asset, :content_type => /\Aimage\/.*\Z/
   do_not_validate_attachment_file_type :asset
+
 end
